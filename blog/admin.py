944bb8147a8dab29ef5b3036297f8aef2aa7e64a
snollygoster123 <snollygoster123@gmail.com>
2015-02-13 23:30:56 +0000

    Updated admin pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from blog.models import Blog, Comment
+
 
 # Register your models here.
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+
+class BlogAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['title', 'text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [CommentInline]
+
+
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
+import datetime
+from django.utils import timezone
+
 
 # Create your models here.
 class Blog(models.Model):
     author = models.CharField(max_length=128)
+    title = models.CharField(max_length=256)
     pub_date = models.DateTimeField('date published')
     ip_address = models.GenericIPAddressField()
     text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Comment(models.Model):
@@ -15,3 +25,6 @@ class Comment(models.Model):
     date = models.DateTimeField('date commented')
     ip_address = models.GenericIPAddressField()
     text = models.TextField()
+
+    def __str__(self):
+        return self.text
